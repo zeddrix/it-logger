20d943da8e89b447905304816b232c22726c407f
Zeddrix Fabian <zeddrix.fabian@gmail.com>
2021-01-06 05:47:17 +0800

    Created techs reducer, action, and component.

--- src/actions/logActions.js
+++ src/actions/logActions.js
@@ -24,7 +24,7 @@ export const getLogs = () => async dispatch => {
   } catch (err) {
     dispatch({
       type: LOGS_ERROR,
-      payload: err.response.data
+      payload: err.response.statusText
     })
   }
 };
@@ -49,7 +49,7 @@ export const addLog = (log) => async dispatch => {
   } catch (err) {
     dispatch({
       type: LOGS_ERROR,
-      payload: err.response.data
+      payload: err.response.statusText
     })
   }
 };
@@ -69,7 +69,7 @@ export const deleteLog = (id) => async dispatch => {
   } catch (err) {
     dispatch({
       type: LOGS_ERROR,
-      payload: err.response.data
+      payload: err.response.statusText
     })
   }
 };
@@ -96,7 +96,7 @@ export const updateLog = log => async dispatch => {
   } catch (err) {
     dispatch({
       type: LOGS_ERROR,
-      payload: err.response.data
+      payload: err.response.statusText
     })
   }
 };
@@ -116,7 +116,7 @@ export const searchLogs = (text) => async dispatch => {
   } catch (err) {
     dispatch({
       type: LOGS_ERROR,
-      payload: err.response.data
+      payload: err.response.statusText
     })
   }
 };
