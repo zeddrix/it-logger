1f8c02da4d5d44e7b062a54daaa7b5c4277dab76
Zeddrix Fabian <zeddrix.fabian@gmail.com>
2021-01-06 05:09:46 +0800

    Able to update logs.

--- src/components/logs/EditLogModal.js
+++ src/components/logs/EditLogModal.js
@@ -1,16 +1,37 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import M from 'materialize-css/dist/js/materialize.min.js';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { updateLog } from '../../actions/logActions';
 
-const EditLogModal = () => {
+const EditLogModal = ({ current, updateLog }) => {
   const [message, setMessage] = useState('');
   const [attention, setAttention] = useState(false);
   const [tech, setTech] = useState('');
+
+  useEffect(() => {
+    if (current) {
+      setMessage(current.message);
+      setAttention(current.attention);
+      setTech(current.tech);
+    }
+  }, [current]);
 
   const onSubmit = () => {
     if (message === '' || tech === '') {
       M.toast({ html: 'Please enter a message and tech' })
     } else {
-      console.log(message, tech, attention);
+      const updLog = {
+        id: current.id,
+        message,
+        attention,
+        tech,
+        date: new Date()
+      };
+
+      updateLog(updLog);
+      M.toast({ html: `Log updated by ${tech}` })
+
       setMessage('');
       setTech('');
       setAttention(false);
@@ -29,9 +50,6 @@ const EditLogModal = () => {
               value={message}
               onChange={e => setMessage(e.target.value)}
             />
-            <label htmlFor="message" className='active'>
-              Log Message
-            </label>
           </div>
         </div>
         <div className="row">
@@ -83,6 +101,16 @@ const EditLogModal = () => {
 const modalStyle = {
   width: '75%',
   height: '75%'
+};
+
+EditLogModal.propTypes = {
+  current: PropTypes.object,
+  updateLog: PropTypes.func.isRequired,
 }
 
-export default EditLogModal;
+const mapStateToProps = state => ({
+  current: state.log.current
+});
+
+export default connect(mapStateToProps, { updateLog })
+  (EditLogModal);
--- src/components/logs/LogItem.js
+++ src/components/logs/LogItem.js
@@ -2,11 +2,11 @@ import React from 'react';
 import Moment from 'react-moment';
 import { connect } from 'react-redux'
 import PropTypes from 'prop-types';
-import { deleteLog } from '../../actions/logActions';
+import { deleteLog, setCurrent } from '../../actions/logActions';
 
 import M from 'materialize-css/dist/js/materialize.min.js';
 
-const LogItem = ({ log, deleteLog }) => {
+const LogItem = ({ log, deleteLog, setCurrent }) => {
   const onDelete = () => {
     deleteLog(log.id);
     M.toast({ html: 'Log deleted' })
@@ -18,7 +18,10 @@ const LogItem = ({ log, deleteLog }) => {
         <a
           href="#edit-log-modal"
           className={`modal-trigger ${log.attention ? 'red-text' : 'blue-text'
-            }`}>{log.message}
+            }`}
+          onClick={() => setCurrent(log)}
+        >
+          {log.message}
         </a>
         <br />
         <span className="grey-text">
@@ -39,7 +42,8 @@ const LogItem = ({ log, deleteLog }) => {
 LogItem.propTypes = {
   log: PropTypes.object.isRequired,
   deleteLog: PropTypes.func.isRequired,
+  setCurrent: PropTypes.func.isRequired,
 };
 
-export default connect(null, { deleteLog })
+export default connect(null, { deleteLog, setCurrent })
   (LogItem);
