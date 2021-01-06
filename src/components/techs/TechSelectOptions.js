2f71774aa9f4343e44f2106909529e2609438cf3
Zeddrix Fabian <zeddrix.fabian@gmail.com>
2021-01-06 11:22:35 +0800

    Able to delete techs.

--- src/components/techs/TechSelectOptions.js
+++ src/components/techs/TechSelectOptions.js
@@ -7,7 +7,7 @@ const TechSelectOptions = ({ getTechs, tech: { techs, loading } }) => {
   useEffect(() => {
     getTechs();
     // es-lint-disable-next-line
-  }, [])
+  }, [getTechs])
   return (
     !loading && techs !== null && techs.map(
       t => <option key={t.id} value={`${t.firstName} ${t.lastName}`}>
