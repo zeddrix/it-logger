191547b6c43a9ae4380a2a90962838b74cba65b2
Zeddrix Fabian <zeddrix.fabian@gmail.com>
2021-01-19 07:41:50 +0800

    Assigned reducers to var before exporting.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,8 @@
 import { combineReducers } from 'redux';
-import logReducer from './logReducer';
-import techReducer from './techReducer';
+import { logReducer } from './logReducer';
+import { techReducer } from './techReducer';
 
 export default combineReducers({
-  log: logReducer,
-  tech: techReducer
+	log: logReducer,
+	tech: techReducer,
 });
